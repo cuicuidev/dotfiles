b83ffe142fa080467930461f7142d09f886ae0e7
Dmitry Ryzhenkov <dmitryryzhenkov@cuicui.dev>
2024-01-10 21:19:24 +0100

    initial commit

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+vim.cmd("set autoindent")
+vim.g.mapleader = " "
+
+vim.cmd("map <C-n> :Neotree toggle<CR>")
